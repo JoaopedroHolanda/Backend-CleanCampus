b297227f0269e64e9f871a07467feab47af04dca
João Pedro <126481770+JoaopedroHolanda@users.noreply.github.com>
2024-12-03 22:18:17 -0300

    Colocando documentação da API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const routes = require('./routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json'); 
 
 const app = express()
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 routes(app)
+
 
 
 module.exports = app
